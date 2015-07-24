cd88d824dc2c76492dde7c2f24bc603075d02bf3
Stephen Young <stephen.young.1030@gmail.com>
2015-07-24 14:47:01 +1200

    L1, HFJ, Clock.java.

---
+++ Lectures/Lecture01/Clock.java
@@ -0,0 +1,25 @@
+/*	HFJ
+ * 	P88	
+ */
+
+class Clock {
+	String time;
+	
+	void setTime(String t)	{
+	time = t;
+	}
+	
+	String getTime()	{
+		return time;
+	}
+}
+
+class ClockTestDrive	{
+	public static void main(String[] args)	{
+		Clock c = new Clock();
+		
+		c.setTime("7:00");
+		String tod = c.getTime();
+		System.out.println("time: " + tod);
+	}
+}
