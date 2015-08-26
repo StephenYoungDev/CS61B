3a9bb304c33eed2fdffecd69a7964820c60b32ec
Stephen Young <stephen.young.1030@gmail.com>
2015-08-27 10:31:19 +1200

    Add code from HFJ_P84.

---
+++ Lectures/Lecture01/PoorDog.java
@@ -0,0 +1,13 @@
+class PoorDog {
+    private int size;   //Declare two instance variables, but don't assign a value.
+    private String name;
+
+    public int getSize() {
+	return size;
+    }
+
+    public String getName() {
+	return name;
+    }
+}
+
