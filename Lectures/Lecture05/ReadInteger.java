18b82b052d5b72de4b3de81f7e49b60017f2a510
Stephen Young <stephen.young.1030@gmail.com>
2015-09-02 11:46:21 +1200

    Updated code for ReadInteger.java.

--- Lectures/Lecture05/ReadInteger.java
+++ Lectures/Lecture05/ReadInteger.java
@@ -1,23 +1,24 @@
 import java.io.*;
 
-/* Use BufferedReader and check NumberFormatException. */
+/* Use static method parseInt() from Integer class. BufferedReader to read inputfrom system and check NumberFormatException from first input. */
 
 public class ReadInteger{
     public static void main(String[] args) throws Exception {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         boolean check;
         int i = 0;
-        System.out.print("Integer: ");
+        System.out.print("Please Enter an Integer: ");
         do{
             check = true;
             try{
                 i = Integer.parseInt(in.readLine());
             } catch (NumberFormatException e){
-                System.err.println("Input error - Invalid value for an int.");
+                System.err.println("Input error - Invalid value for an Integer.");
                 System.out.print("Reinsert: ");
                 check = false;
             }
         } while (!check);
-        System.out.print(i + " + 7 = " + (i+7));
+        System.out.print("The Integer you entered is : " + i);
     }
 }
+
