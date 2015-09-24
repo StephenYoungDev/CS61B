9711a6942c3c415708abab9b7cab3fdbf3c4c6ce
Stephen Young <stephen.young.1030@gmail.com>
2015-09-24 16:18:58 +1200

    How constructors work. HFJ_p257.

---
+++ Lectures/Lecture10/superclassconstructor/Boo.java
@@ -0,0 +1,6 @@
+public class Boo {
+    public Boo(int i) { }
+    public Boo(String s) { }
+    public Boo(String s, int i) { }
+}
+// HFJ_p257
---
+++ Lectures/Lecture10/superclassconstructor/SonOfBoo.java
@@ -0,0 +1,30 @@
+class SonOfBoo extends Boo {
+
+    public SonOfBoo() {
+	super("boo");
+    }
+
+    public SonOfBoo(int i) {
+	super("Fred");
+    }
+
+    public SonOfBoo(String s) {
+	super(42);
+    }
+    
+    /*
+    public SonOfBoo(int i, String s) {
+    }
+    */
+
+    public SonOfBoo(int i, int j) {
+	super("man", j);
+    }
+
+    /*
+    public SonOfBoo(int i, int x, int y) {
+	super(i, "star");
+    }
+    */
+}
+// HFJ_p257
