1b08c2e084cc7e86829bb500d8846a5e494d0d35
Stephen Young <Stephen Young>
2016-01-21 15:48:22 +1300

    Code for exceptions. HFJ_P321.

---
+++ Lectures/Lecture15/MusicTest1.java
@@ -0,0 +1,13 @@
+import javax.sound.midi.*;
+
+public class MusicTest1 {
+    public void play() {
+	Sequencer sequence = new Sequencer();
+    }
+
+    public static void main(String[] args) {
+	MusicTest1  mt = new MusicTest1();
+	mt.play();
+    }
+}
+
